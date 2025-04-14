2b8febecc3ffaa6b4aa57e64eda32790d5e0ce1b
cargo <mohamed2003torky@gmail.com>
2025-04-15 05:18:38 +0800

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <div>
-      <h1></h1>
+      <Routes>
+
+      </Routes>
     </div>
   )
 }
