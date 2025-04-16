ca936d0c57fc099df9ab8df2a7bc0e10ad49de9e
cargo <mohamed2003torky@gmail.com>
2025-04-17 01:33:24 +0800

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div>
-      <h1></h1>
+      <Navbar />
+      <Routes >
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
