25090a01f1bb74a7681db36f99586c1c85f86e0d
cargo <mohamed2003torky@gmail.com>
2025-04-16 14:07:09 +0800

    Added Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
+import Styles from './pages/Styles'
 
 function App() {
 
   return (
     <div>
       <Routes>
-
+        <Route path="/styles" element={<Styles />} />
       </Routes>
     </div>
   )
